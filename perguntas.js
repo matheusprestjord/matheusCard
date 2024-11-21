8b065cd44f1cfd3f4fa1d03051e64e541a9076de
Matheus Prestjord <122492038+matheusprestjord@users.noreply.github.com>
2024-11-21 18:42:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'História',
+    'Em que ano o Brasil se tornou independente?',
+    'Em 7 de setembro de 1822'
+)
+
+criaCartao(
+    'Filosofia',
+    'Quem é o pai da filosofia?',
+    'Sócrates'
+)
+
+criaCartao(
+    'Matemática',
+    'Como descobrir o volume de um cone?',
+    'V=1/3hπr²'
+)
+
+criaCartao(
+    'Lingua portuguesa',
+    'O que é um pleonasmo?',
+    'É uma superabundância de palavras que expressam uma mesma ideia em uma frase'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital do Chile?',
+    'Santiago'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é o nome da organela que ocorre a respiração celular?',
+    'Mitocôndria'
+)
